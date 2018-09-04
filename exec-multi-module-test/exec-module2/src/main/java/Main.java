32289a73c79b0e5f0b32e6d37cfe70595877c5b9
andxu <andxu@microsoft.com>
2018-09-04 11:13:20 +0800

    add System.in.read to make the console program don't exit in azds env.

--- exec-multi-module-test/exec-module2/src/main/java/Main.java
+++ exec-multi-module-test/exec-module2/src/main/java/Main.java
@@ -1,9 +1,11 @@
 public final class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         if (args.length > 0) {
             System.out.println("args[0] is " + args[0]);
         }
         
         new MyPrinter().printHello();
+
+        System.in.read();
     }
 }
