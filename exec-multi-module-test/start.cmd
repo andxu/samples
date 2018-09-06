64d05f49bb5137ae588d8b184d426e89bf80f922
Andy Xu(devdiv) <andxu@microsoft.com>
2018-09-06 16:22:14 +0800

    update tools.jar path

--- exec-multi-module-test/start.cmd
+++ exec-multi-module-test/start.cmd
@@ -1,2 +1,2 @@
 set HOME=%HOMEPATH%
-java -cp .\javadebug\com.microsoft.java.debug.core-0.1.1-jar-with-dependencies.jar;%JAVA_HOME%/lib/tools.jar com.microsoft.java.debug.core.server.JavaDebugServer
+java -cp .\javadebug\com.microsoft.java.debug.core-0.1.1-jar-with-dependencies.jar;"%JAVA_HOME%/lib/tools.jar" com.microsoft.java.debug.core.server.JavaDebugServer
