a6eae87fdc3667b8ddf8e94c1a9f2e0f76ce8edf
andxu <andxu@microsoft.com>
2018-11-07 13:15:35 +0800

    remove templates that are auto-generated.

--- exec-multi-module-test/start.cmd
+++
@@ -1,2 +0,0 @@
-set HOME=%HOMEPATH%
-java -cp .\javadebug\com.microsoft.java.debug.core-0.1.1-jar-with-dependencies.jar;"%JAVA_HOME%/lib/tools.jar" com.microsoft.java.debug.core.server.JavaDebugServer
